2fd337c610f8b562f8dd2f365e909a599ca263b9
andrepology <49971348+andrepology@users.noreply.github.com>
2025-11-11 10:44:53 -0800

    final fog tweak

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ export default function App() {
         <color attach="background" args={['#F6F5F3']} />
         <AdaptiveFog
           color="#F6F5F3"
-          defaultFog={{ near: 6, far: 12 }}
+          defaultFog={{ near: 6, far: 20 }}
           focusedFog={{ near: 4, far: 12 }}
           animationDuration={1.2}
         />
