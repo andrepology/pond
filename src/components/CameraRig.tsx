57f8a9cbdb35fb51557ed020476f7cf215f1dfe5
andrepology <49971348+andrepology@users.noreply.github.com>
2025-11-11 10:30:12 -0800

    attempt inset fix

--- src/components/CameraRig.tsx
+++ src/components/CameraRig.tsx
@@ -97,8 +97,8 @@ export function CameraRig({ isJournalDocked }: CameraRigProps) {
       smoothTime={0.8}
       draggingSmoothTime={0.2}
       enabled={true}
-      dollySpeed={isMobileDevice() ? 3.5 : 0.5}
-      truckSpeed={isMobileDevice() ? 3.5 : 0.5}
+      dollySpeed={isMobileDevice() ? 1.5 : 0.5}
+      truckSpeed={isMobileDevice() ? 1.5 : 0.5}
       
       touches={{
         one: ACTION.ACTION.TOUCH_ROTATE,
